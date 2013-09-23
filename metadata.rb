473a1e7320793ec578a8a1279ccbfe92d89fd772
rackerjoe <breu@breu.org>
2013-09-23 13:49:23 -0500

    metadata.rb cleanup

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name             'memcached-openstack'
-maintainer       'Rackspace US, Inc.'
-license          'Apache 2.0'
-description      'Installs/Configures memcached-openstack'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+name             "memcached-openstack"
+maintainer       "Rackspace US, Inc."
+license          "Apache 2.0"
+description      "Installs/Configures memcached-openstack"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "0.1.0"
 
 %w{ centos ubuntu }.each do |os|
   supports os
